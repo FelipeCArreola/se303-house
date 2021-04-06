d0dd87cfd5aba16938ff6c3d259d5b49a9c0baf6
Felipe Arreola <arreolaf@oregonstate.edu>
2021-04-06 10:23:13 -0700

    Added function of 'line(var)' in House Class, added basic skeleton format to output string for case 1..12

---
+++ lib/house.rb
@@ -0,0 +1,33 @@
+class House
+
+def line(number)
+  case number
+  when 1
+    "This is the house that Jack built.\n"
+  when 2
+    "This is the malt that lay in the house that Jack built.\n"
+  when 3
+    #TODO
+  when 4
+    #TODO
+  when 5
+    #TODO
+  when 6
+    #TODO
+  when 7
+    #TODO
+  when 8
+    #TODO
+  when 9
+    #TODO
+  when 10
+    #TODO
+  when 11
+    #TODO
+  when 12
+    #TODO
+  else
+    #TODO
+end
+
+end
