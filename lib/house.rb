50712cfdf9fd4feb55e8186027cf790f484af4ec
Felipe Arreola <arreolaf@oregonstate.edu>
2021-05-30 11:13:44 -0700

    made case when 1 branch similar to the other branches, added a method '_line_segment_1' which does not really provide any contents to the string itself, but provides for the other branches (lines 2..12) a space when concated together. there is now a uniform appearance across branches '1..12'

--- lib/house.rb
+++ lib/house.rb
@@ -2,29 +2,29 @@ class House
   def line(number)
     case number
     when 1
-      "This is the house that Jack built.\n"
+      "This is #{_construct_line(1)}the house that Jack built.\n"
     when 2
-      "This is #{_construct_line(2)} the house that Jack built.\n"
+      "This is #{_construct_line(2)}the house that Jack built.\n"
     when 3
-      "This is #{_construct_line(3)} the house that Jack built.\n"
+      "This is #{_construct_line(3)}the house that Jack built.\n"
     when 4
-      "This is #{_construct_line(4)} the house that Jack built.\n"
+      "This is #{_construct_line(4)}the house that Jack built.\n"
     when 5
-      "This is #{_construct_line(5)} the house that Jack built.\n"
+      "This is #{_construct_line(5)}the house that Jack built.\n"
     when 6
-      "This is #{_construct_line(6)} the house that Jack built.\n"
+      "This is #{_construct_line(6)}the house that Jack built.\n"
     when 7
-      "This is #{_construct_line(7)} the house that Jack built.\n"
+      "This is #{_construct_line(7)}the house that Jack built.\n"
     when 8
-      "This is #{_construct_line(8)} the house that Jack built.\n"
+      "This is #{_construct_line(8)}the house that Jack built.\n"
     when 9
-      "This is #{_construct_line(9)} the house that Jack built.\n"
+      "This is #{_construct_line(9)}the house that Jack built.\n"
     when 10
-      "This is #{_construct_line(10)} the house that Jack built.\n"
+      "This is #{_construct_line(10)}the house that Jack built.\n"
     when 11
-      "This is #{_construct_line(11)} the house that Jack built.\n"
+      "This is #{_construct_line(11)}the house that Jack built.\n"
     when 12
-      "This is #{_construct_line(12)} the house that Jack built.\n"
+      "This is #{_construct_line(12)}the house that Jack built.\n"
     else
       "ERROR: OUT OF RANGE, ONLY \'1..12\'"
     end
@@ -35,6 +35,10 @@ class House
   end
 
   private
+
+  def _line_segment_1
+    ""
+  end
 
   def _line_segment_2
     "the malt that lay in"
@@ -81,7 +85,7 @@ class House
   end
 
   def _collection_of_segmented_line(upper_limit_line_number)
-    upper_limit_line_number.downto(2).collect {|line_segment_num| send("_line_segment_#{line_segment_num}")}
+    upper_limit_line_number.downto(1).collect {|line_segment_num| send("_line_segment_#{line_segment_num}")}
   end
 
   def _construct_line(upper_limit_line_number)
