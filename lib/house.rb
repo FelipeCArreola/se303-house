80cad001d3b675cca5aa8c6f2cd520b24a60ba70
Felipe Arreola <arreolaf@oregonstate.edu>
2021-05-30 11:32:44 -0700

    added an array that is called 'segments' contains all contents from functions '_line_segment_[1..12]'. these were placed in reverse order to utilize the 'last' function of the array class; which returns segments in the proper order that was obsereved. also changed '_construct_line' function: calls the array 'segments' and utilizes the 'last' function to join segments

--- lib/house.rb
+++ lib/house.rb
@@ -1,4 +1,25 @@
 class House
+  attr_reader :segments
+
+  def initialize
+      @segments = [
+        "the horse and the hound and the horn that belonged to",
+        "the farmer sowing his corn that kept",
+        "the rooster that crowed in the morn that woke",
+        "the priest all shaven and shorn that married",
+        "the man all tattered and torn that kissed",
+        "the maiden all forlorn that milked",
+        "the cow with the crumpled horn that tossed",
+        "the dog that worried",
+        "the cat that killed",
+        "the rat that ate",
+        "the malt that lay in",
+        ""
+      ]
+
+  end
+
+
   def line(number)
     case number
     when 1..12
@@ -67,7 +88,8 @@ class House
   end
 
   def _construct_line(upper_limit_line_number)
-    _collection_of_segmented_line(upper_limit_line_number).join(" ")
+    # _collection_of_segmented_line(upper_limit_line_number).join(" ")
+    segments.last(upper_limit_line_number).join(" ")
   end
 
 end
