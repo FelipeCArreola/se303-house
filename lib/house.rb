a9e5c4d86fe87d452602e76bb5c79045636dbd23
Felipe Arreola <arreolaf@oregonstate.edu>
2021-05-30 10:44:14 -0700

    added method that constructs a line based on a collection of segments, removed '_line_segment_1' as it is in all lines

--- lib/house.rb
+++ lib/house.rb
@@ -4,7 +4,7 @@ class House
     when 1
       "This is the house that Jack built.\n"
     when 2
-      "This is #{_line_segment_2} the house that Jack built.\n"
+      "This is #{_construct_line(2)} the house that Jack built.\n"
     when 3
       "This is #{_line_segment_3+" "+_line_segment_2} the house that Jack built.\n"
     when 4
@@ -36,10 +36,6 @@ class House
 
   private
 
-  def _line_segment_1
-    "the house that Jack built"
-  end
-
   def _line_segment_2
     "the malt that lay in"
   end
@@ -55,26 +51,41 @@ class House
   def _line_segment_5
     "the dog that worried"
   end
+
   def _line_segment_6
     "the cow with the crumpled horn that tossed"
   end
+
   def _line_segment_7
     "the maiden all forlorn that milked"
   end
+
   def _line_segment_8
     "the man all tattered and torn that kissed"
   end
+
   def _line_segment_9
     "the priest all shaven and shorn that married"
   end
+
   def _line_segment_10
     "the rooster that crowed in the morn that woke"
   end
+
   def _line_segment_11
     "the farmer sowing his corn that kept"
   end
+
   def _line_segment_12
     "the horse and the hound and the horn that belonged to"
+  end
+
+  def _collection_of_segmented_line(upper_limit_line_number)
+    upper_limit_line_number.downto(2).collect {|line_segment_num| send("_line_segment_#{line_segment_num}")}
+  end
+
+  def _construct_line(upper_limit_line_number)
+    _collection_of_segmented_line(upper_limit_line_number).join(" ")
   end
 
 end
