810b79ccabf42f42cdb7e05839c2caafaaa0bc6d
Felipe Arreola <arreolaf@oregonstate.edu>
2021-05-30 21:40:04 -0700

    shifted segment (data) to a fixed static method. added classes SortSetup, RandomSetup, Setup, to provide for functionality

--- lib/house.rb
+++ lib/house.rb
@@ -1,22 +1,10 @@
 class House
-  attr_reader :segments, :prefix
+  attr_reader :prefix, :setup, :ordered_data
 
   def initialize(args)
-    @segments = {
-      1   =>  "the horse and the hound and the horn that belonged to",
-      2   =>  "the farmer sowing his corn that kept",
-      3   =>  "the rooster that crowed in the morn that woke",
-      4   =>  "the priest all shaven and shorn that married",
-      5   =>  "the man all tattered and torn that kissed",
-      6   =>  "the maiden all forlorn that milked",
-      7   =>  "the cow with the crumpled horn that tossed",
-      8   =>  "the dog that worried",
-      9   =>  "the cat that killed",
-      10  =>  "the rat that ate",
-      11  =>  "the malt that lay in",
-      12  =>  ""
-    } 
     @prefix = Prefix.factory(args[:prefix])
+    @setup  = Setup.factory(args[:setup])
+    @ordered_data = setup.order(args[:data])  
   end
 
   def line(number)
@@ -32,6 +20,23 @@ class House
   
   def recite
     (1..12).collect {|line_number| line(line_number)}.join("\n")
+  end
+
+  def self.segments
+    {
+      1   =>  "the horse and the hound and the horn that belonged to",
+      2   =>  "the farmer sowing his corn that kept",
+      3   =>  "the rooster that crowed in the morn that woke",
+      4   =>  "the priest all shaven and shorn that married",
+      5   =>  "the man all tattered and torn that kissed",
+      6   =>  "the maiden all forlorn that milked",
+      7   =>  "the cow with the crumpled horn that tossed",
+      8   =>  "the dog that worried",
+      9   =>  "the cat that killed",
+      10  =>  "the rat that ate",
+      11  =>  "the malt that lay in",
+      12  =>  ""
+    } 
   end
 
   private
@@ -79,6 +84,48 @@ class Prefix
       TharPrefix
     else
       raise Exception.new("No prefix found for selection: #{px}.\n")
+    end.new
+  end
+
+end
+
+module DataSetupBehavior
+
+  def order(array)
+    raise NoMethodError.new("#{self.class} has not implemented \'order\' for Module: \'DataSetupBehavior\'.\n")
+  end
+end
+
+class SortSetup
+  
+  include DataSetupBehavior
+
+  def order(hash)
+    hash.sort.to_h
+  end
+
+end
+
+class RandomSetup
+  
+  include DataSetupBehavior
+
+  def order(hash)
+    hash.values.shuffle.to_h
+  end
+
+end
+
+class Setup
+
+  def self.factory(setup)
+    case setup
+    when "Sort"
+      SortSetup
+    when "Random"
+      RandomSetup
+    else
+      raise Exception.new("No setup found for selection: #{setup}.\n")
     end.new
   end
 
